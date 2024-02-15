12d191e41a1c4da9fc6b2f8960b52986e18e5adb
v-duma <139060657+v-duma@users.noreply.github.com>
2024-02-15 19:57:37 +0200

    added file 'vercel.json'

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { TiArrowBack } from "react-icons/ti";
 import css from "./NotFound.module.css";
 
 export default function NotFound() {
@@ -7,13 +6,7 @@ export default function NotFound() {
     <div className={css.notFound}>
       <h2>Not found page</h2>
       <div className={css.wrap}>
-        <Link to="/">
-          <TiArrowBack
-            className={css.myIcon}
-            size="24"
-          />
-          Go back
-        </Link>
+        <Link to="/">Go back</Link>
       </div>
     </div>
   );
